0b3239403ddc6ea30404305a455fdbc3f98e81e8
thiago <desenvolvedor.nunes@gmail.com>
2022-12-06 19:53:33 -0300

    refactor: corrige teste de componente de exposicao de item

--- src/__tests__/components/item/item.spec.tsx
+++ src/__tests__/components/item/item.spec.tsx
@@ -1,34 +1,23 @@
-import { Item } from '@components/item';
-import { renderTheme } from '@styles/render-theme';
 import { screen } from '@testing-library/react';
+
+import { Item } from '@components/item/exposition';
+import { renderTheme } from '@styles/render-theme';
 
 describe('<Item />', () => {
   it('should ', () => {
     renderTheme(
-      <Item
-        size={35}
-        title="tênis de caminhada"
-        brand="Nike"
-        price={150}
-        stock={12}
-      />
+      <Item title="tênis de caminhada" price={150} src="/shoes.png" />
     );
 
     const shoesImage = screen.getByAltText('shoes');
-    const size = screen.getByText(/tamanho/gi);
     const title = screen.getByText(/tênis/gi);
-    const brand = screen.getByText(/marca/gi);
-    const stock = screen.getByText(/estoque/gi);
     const price = screen.getByText((content, element) => {
       return content.startsWith('R$');
     });
     const button = screen.getByRole('button');
 
     expect(shoesImage).toBeInTheDocument();
-    expect(size).toBeInTheDocument();
-    expect(stock).toBeInTheDocument();
     expect(title).toBeInTheDocument();
-    expect(brand).toBeInTheDocument();
     expect(button).toBeInTheDocument();
     expect(price).toBeInTheDocument();
   });
