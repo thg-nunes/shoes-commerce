edb5bda3331e0a8b82065bec77f4c481b12007d5
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-12-06 15:54:30 -0300

    style: cria marcacao de estilos para componente item

---
+++ src/components/item/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Details = styled.section``;
+
+export const Title = styled.title``;
+
+export const Price = styled.p``;
+
+export const ShoesDetailsSection = styled.section``;
