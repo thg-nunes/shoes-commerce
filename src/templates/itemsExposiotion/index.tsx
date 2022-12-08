eb73c4b59231530c7d7154826986b06e3cfd488e
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-12-08 18:00:27 -0300

    feat: adiciona componente de paginacao nos items de exposicao

--- src/templates/itemsExposiotion/index.tsx
+++ src/templates/itemsExposiotion/index.tsx
@@ -1,34 +1,59 @@
 import { ExpositionItem } from '@components/item/exposition';
+import { Pagination } from '@components/pagination';
+import { useEffect, useState } from 'react';
 import * as Styled from './styled';
 
+type ItemsList = {
+  id: string;
+  brand: string;
+  title: string;
+  description: string;
+  image: string;
+  size: number;
+  stock: number;
+  price: number;
+}[];
+
 export type ItemsExpositionProps = {
-  itemsList: {
-    id: string;
-    brand: string;
-    title: string;
-    description: string;
-    image: string;
-    size: number;
-    stock: number;
-    price: number;
-  }[];
+  itemsList: ItemsList;
 };
 
 export function ItemsExposition({
   itemsList,
 }: ItemsExpositionProps): JSX.Element {
+  const [items] = useState(itemsList);
+  const [itemsOfThePage, setItemsOfThePage] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(12);
+
+  useEffect(() => {
+    const init_slice = currentPage === 1 ? 0 : itemsPerPage;
+    const end_slice = currentPage === 1 ? itemsPerPage : itemsPerPage * 2;
+
+    const itemsToExposition = items.slice(init_slice, end_slice);
+
+    setItemsOfThePage(itemsToExposition);
+  }, [currentPage]);
+
   return (
     <Styled.Container>
-      {itemsList !== undefined &&
-        itemsList.map((item) => (
-          <ExpositionItem
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            price={item.price}
-            src={item.image}
-          />
-        ))}
+      <section>
+        {itemsOfThePage.length &&
+          itemsOfThePage.map((item) => (
+            <ExpositionItem
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              price={item.price}
+              src={item.image}
+            />
+          ))}
+      </section>
+      <Pagination
+        pageActive={currentPage}
+        totalPage={itemsList.length / 12}
+        setPage={setCurrentPage}
+      />
     </Styled.Container>
   );
 }
