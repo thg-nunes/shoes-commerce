1075d0d7d3bbf49a72df3a8672564e60b4177774
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-12-08 18:00:04 -0300

    style: estilo do template de itens de exposicao

--- src/templates/itemsExposiotion/styled.ts
+++ src/templates/itemsExposiotion/styled.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  > section {
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+
+  max-width: 1280px;
+  margin: 0 auto;
+`;
+
+export const ButtonsSecton = styled.section``;
