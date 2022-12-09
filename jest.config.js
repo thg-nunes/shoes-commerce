869b41e51effcb4dab6e14a16d0a334d1cd3f522
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-12-09 15:08:03 -0300

    test:

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
     '!src/**/*_app.tsx',
     '!src/**/*_document.tsx',
     '!<rootDir>/src/pages/api/**/*.{js,jsx,ts,tsx}',
-    '!<rootDir>/src/**/*.{js,ts}',
+    '!<rootDir>/src/components/**/*.{js,ts}',
     '!<rootDir>/src/styles/**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/services  /**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/**/*.stories.{js,jsx,ts,tsx}',
---
+++ src/__tests__/components/filters/default/filter.spec.tsx
@@ -0,0 +1,67 @@
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+
+import { searchItem } from '@utils/searchItem';
+import { DefaultFilter } from '@components/filters/default';
+
+import { renderTheme } from '@styles/render-theme';
+
+jest.mock('@utils/searchItem', () => {
+  return {
+    searchItem: jest.fn().mockResolvedValue({
+      status: 'success',
+      items: [
+        {
+          id: '1',
+          brand: 'Nike',
+          title: 'any title',
+          description: 'any description',
+          image: 'any image',
+          size: 36,
+          stock: 5,
+          color: 'red',
+          price: 10,
+        },
+      ],
+    }),
+  };
+});
+
+const searchItemMock = searchItem as jest.Mock;
+
+describe('', () => {
+  it('ensures that the request to get items from filter default select is execute correctly', async () => {
+    const setItems = jest.fn();
+
+    renderTheme(
+      <DefaultFilter filterOf="brand" textFilter="Nike" setItems={setItems} />
+    );
+
+    const optionColor = screen.getByRole('listitem');
+
+    expect(optionColor).toBeInTheDocument();
+
+    fireEvent.click(optionColor);
+
+    await waitFor(() =>
+      expect(searchItemMock).toHaveBeenCalledWith({
+        searchBy: 'Nike',
+        filterOf: 'brand',
+      })
+    );
+    await waitFor(() =>
+      expect(setItems).toHaveBeenCalledWith([
+        {
+          id: '1',
+          brand: 'Nike',
+          title: 'any title',
+          description: 'any description',
+          image: 'any image',
+          size: 36,
+          stock: 5,
+          color: 'red',
+          price: 10,
+        },
+      ])
+    );
+  });
+});
