8710668568d247e6ce41b286cf6e4488537f0c62
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-12-10 13:35:47 -0300

    feat: marcacao do estilo e pagina cart

---
+++ src/styles/pages/cart/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.main``;
+
+export const Table = styled.table``;
+
+export const TableHead = styled.thead``;
+
+export const TableRow = styled.tr``;
+
+export const TableData = styled.td``;
