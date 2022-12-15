f1ef2574542ef2f76690cafdcaf1b34c91ec5f85
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-12-15 12:24:31 -0300

    style: estilo do loading e mensagem caso nao haja item por selecao de filtro

--- src/styles/pages/homePage/styled.ts
+++ src/styles/pages/homePage/styled.ts
@@ -86,3 +86,22 @@ export const BrandFilters = styled.section`
 export const SizeFilters = styled(BrandFilters)``;
 
 export const ColorFilters = styled(BrandFilters)``;
+
+export const MessageInfo = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 93vh;
+
+  a {
+    font-size: 1rem;
+    font-weight: 600;
+    text-decoration: underline;
+    transition: 100ms all ease-in-out;
+
+    :hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
