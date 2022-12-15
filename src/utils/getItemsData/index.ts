219ec29a7c0f81499a9687f3d07a0a9ec61ad422
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-12-15 13:05:56 -0300

    refactor: coloca todos os utilitarios relacionados aos itens em um arquivo unico

---
+++ src/utils/getItemsData/index.ts
@@ -0,0 +1,116 @@
+import { useEffect, useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
+
+import { api } from '@services/axios';
+import { ItemsOfTheCart } from '@contexts/shoppingCart/cart';
+
+import { ItemsList } from '@templates/itemsExposiotion';
+
+type GetItemQuantityInListParams = {
+  id: string;
+  itemsSavedInStorage: ItemsOfTheCart[];
+};
+
+type SearchItemProps = {
+  searchBy: string;
+  filterOf: 'brand' | 'size' | 'color';
+};
+
+type Response = {
+  status: 'success' | 'error';
+  items: ItemsList;
+};
+
+type SearchItemByIdProps = {
+  id: string | string[];
+};
+
+type SetTimeToDisplayItems = {
+  _timeToDisplayItems: boolean;
+  setTimeToDisplay: Dispatch<SetStateAction<boolean>>;
+};
+
+type UseReturnTotalPriceOfItemsParams = {
+  itemsData: ItemsList;
+};
+
+const getItemQuantityInList = ({
+  id,
+  itemsSavedInStorage = [],
+}: GetItemQuantityInListParams): number => {
+  const itemOfList = itemsSavedInStorage.find((item) => item.id === id);
+
+  return itemOfList?.quantity;
+};
+
+const searchItem = async ({
+  searchBy,
+  filterOf,
+}: SearchItemProps): Promise<Response> => {
+  const { data } = await api.get<Response>(
+    `/api/search/${filterOf}/${searchBy}`
+  );
+
+  return data;
+};
+
+const searchItemById = async ({
+  id,
+}: SearchItemByIdProps): Promise<Response> => {
+  try {
+    const { data } = await api.get<Response>(`/api/item/${id}`);
+
+    return data;
+  } catch {
+    return { status: 'success', items: [] };
+  }
+};
+
+const timeToDisplayItems = ({
+  _timeToDisplayItems,
+  setTimeToDisplay,
+}: SetTimeToDisplayItems): void => {
+  setTimeToDisplay(!_timeToDisplayItems);
+
+  setTimeout(() => {
+    setTimeToDisplay(true);
+  }, 750);
+};
+
+const useReturnTotalPriceOfItems = ({
+  itemsData,
+}: UseReturnTotalPriceOfItemsParams): string => {
+  const [totalPrice, setTotalPrice] = useState('');
+
+  useEffect(() => {
+    const userListItems = localStorage.getItem('user@listItems');
+    const userListItemsInJson: ItemsOfTheCart[] = JSON.parse(userListItems);
+
+    if (itemsData.length) {
+      let total = 0;
+
+      itemsData.forEach((item, index) => {
+        if (item.id === userListItemsInJson[index].id) {
+          total += Number(item.price) * userListItemsInJson[index].quantity;
+        }
+      });
+
+      const totalPriceInBRL = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(total);
+
+      setTotalPrice(totalPriceInBRL);
+    }
+  }, [itemsData]);
+
+  return totalPrice;
+};
+
+export {
+  searchItem,
+  searchItemById,
+  timeToDisplayItems,
+  getItemQuantityInList,
+  useReturnTotalPriceOfItems,
+};
