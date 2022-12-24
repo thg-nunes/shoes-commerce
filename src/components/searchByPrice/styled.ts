433e73d8232c7a5a5ae53484099c6f08e2a2b2b8
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-12-24 10:27:31 -0300

    style: mudando estilo de botao de pesquisa de item por preco

--- src/components/searchByPrice/styled.ts
+++ src/components/searchByPrice/styled.ts
@@ -1,3 +1,47 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import { Button } from '@components/button/paginationComponent/styled';
+
+export const Container = styled.div`
+  position: relative;
+
+  form {
+    position: absolute;
+
+    height: 0;
+    overflow: hidden;
+
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    background: ${({ theme }) => theme.colors.pink[700]};
+
+    label {
+      height: 2rem;
+
+      display: flex;
+      justify-content: space-between;
+      gap: 0.5rem;
+      line-height: 2rem;
+    }
+
+    input {
+      max-width: 6rem;
+
+      color: ${({ theme }) => theme.colors.gray[400]};
+      padding-left: 0.5rem;
+
+      border: none;
+      border-radius: 2px;
+
+      outline-color: ${({ theme }) => theme.colors.pink[300]};
+    }
+
+    ${Button} {
+      :hover {
+        background: ${({ theme }) => theme.colors.pink[300]};
+      }
+    }
+  }
+`;
