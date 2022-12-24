803275bddfafd1cc5d88f0ae52dad68c7ec45a8f
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-12-24 10:28:21 -0300

    feat: botao para iniciar a pesquisa de itens por preco

--- src/components/searchByPrice/index.tsx
+++ src/components/searchByPrice/index.tsx
@@ -1,34 +1,52 @@
-import { searchByPrice } from '@utils/getItemsData';
 import { useState } from 'react';
+
+import { useItemsBySearchContext } from '@contexts/itemsBySearchForm';
+
+import { searchByPrice } from '@utils/getItemsData';
+
+import { Button } from '@components/button/paginationComponent/styled';
+
 import * as Styled from './styled';
 
 export function FormSearchByPrice(): JSX.Element {
   const [lowestPrice, setLowestPrice] = useState(0);
   const [highestPrice, setHighestPrice] = useState(0);
+  const { setItemsBySearch } = useItemsBySearchContext();
 
   return (
     <Styled.Container>
       <form
-        onSubmit={(e) => {
+        onSubmit={async (e) => {
           e.preventDefault();
 
-          searchByPrice({ lowestPrice, highestPrice });
+          const { items } = await searchByPrice({ lowestPrice, highestPrice });
+
+          setItemsBySearch(items);
         }}
       >
-        <input
-          type="text"
-          name="lowestPrice"
-          placeholder={lowestPrice !== 0 ? String(lowestPrice) : 'de'}
-          value={lowestPrice}
-          onChange={(e) => setLowestPrice(Number(e.target.value))}
-        />
-        <input
-          type="text"
-          name="biggestPrice"
-          placeholder={lowestPrice !== 0 ? String(highestPrice) : 'até'}
-          value={highestPrice}
-          onChange={(e) => setHighestPrice(Number(e.target.value))}
-        />
+        <label htmlFor="lowestPrice">
+          de:
+          <input
+            type="text"
+            id="lowestPrice"
+            name="lowestPrice"
+            placeholder={lowestPrice !== 0 ? String(lowestPrice) : 'de'}
+            value={lowestPrice}
+            onChange={(e) => setLowestPrice(Number(e.target.value))}
+          />
+        </label>
+        <label htmlFor="biggestPrice">
+          até:
+          <input
+            type="text"
+            id="biggestPrice"
+            name="biggestPrice"
+            placeholder={lowestPrice !== 0 ? String(highestPrice) : 'até'}
+            value={highestPrice}
+            onChange={(e) => setHighestPrice(Number(e.target.value))}
+          />
+        </label>
+        <Button type="submit">Pesquisar</Button>
       </form>
     </Styled.Container>
   );
