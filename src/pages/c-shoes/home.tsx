12beeb4c2d4b13ba3868dc45f37989bb31cb32bd
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-01-19 14:01:06 -0300

    feat: remove criacao dos filtros da homepage para um template de filtros

--- src/pages/c-shoes/home.tsx
+++ src/pages/c-shoes/home.tsx
@@ -1,20 +1,17 @@
+import axios from 'axios';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
 
-import axios from 'axios';
 import { useItemsBySearchContext } from '@contexts/itemsBySearchForm';
 
 import { ItemsExposition, ItemsList } from '@templates/itemsExposiotion';
+import { FiltersTemplate } from '@templates/filters';
 
-import { BrandFilter } from '@components/filters/brand';
-import { FormSearchByPrice } from '@components/searchByPrice';
 import { Loading } from '@components/loading';
-import { SizeFilter } from '@components/filters/size';
-import { ColorFilter } from '@components/filters/color';
 import { Footer } from '@components/footer';
+import { MessageAndLink } from '@components/messageAndLink';
 
 import * as Styled from '@styles/pages/homePage/styled';
-import { MessageAndLink } from '@components/messageAndLink';
 
 export default function Home(): JSX.Element {
   const [items, setItems] = useState<ItemsList>([]);
@@ -53,266 +50,11 @@ export default function Home(): JSX.Element {
       </Head>
       {items.length !== 0 ? (
         <Styled.Container>
-          <Styled.FilterSection>
-            <span>Filtros |</span>
-
-            <div>
-              <Styled.BrandFilters>
-                <p>Marca</p>
-                <ul>
-                  <BrandFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    setItems={setItems}
-                    filterOf="brand"
-                    searchBy="Nike"
-                    textContent="Nike"
-                  />
-                  <BrandFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    setItems={setItems}
-                    filterOf="brand"
-                    searchBy="Adidas"
-                    textContent="Adidas"
-                  />
-                  <BrandFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    setItems={setItems}
-                    filterOf="brand"
-                    searchBy="Vr"
-                    textContent="Vr"
-                  />
-                  <BrandFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    setItems={setItems}
-                    filterOf="brand"
-                    searchBy="Asics"
-                    textContent="Asics"
-                  />
-                  <BrandFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    setItems={setItems}
-                    filterOf="brand"
-                    searchBy="Oakley"
-                    textContent="Oakley"
-                  />
-                  <BrandFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    setItems={setItems}
-                    filterOf="brand"
-                    searchBy="Mizuno"
-                    textContent="Mizuno"
-                  />
-                  <BrandFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    setItems={setItems}
-                    filterOf="brand"
-                    searchBy="Skechers"
-                    textContent="Skechers"
-                  />
-                  <BrandFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    setItems={setItems}
-                    filterOf="brand"
-                    searchBy="Mizuno"
-                    textContent="Mizuno"
-                  />
-                  <BrandFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    setItems={setItems}
-                    filterOf="brand"
-                    searchBy="Olympikus"
-                    textContent="Olympikus"
-                  />
-                </ul>
-              </Styled.BrandFilters>
-
-              <Styled.SizeFilters>
-                <p>Tamanho</p>
-                <ul>
-                  <SizeFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="size"
-                    setItems={setItems}
-                    searchBy="26"
-                    textContent="26"
-                  />
-                  <SizeFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="size"
-                    setItems={setItems}
-                    searchBy="28"
-                    textContent="28"
-                  />
-                  <SizeFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="size"
-                    setItems={setItems}
-                    searchBy="30"
-                    textContent="30"
-                  />
-                  <SizeFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="size"
-                    setItems={setItems}
-                    searchBy="32"
-                    textContent="32"
-                  />
-                  <SizeFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="size"
-                    setItems={setItems}
-                    searchBy="34"
-                    textContent="34"
-                  />
-                  <SizeFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="size"
-                    setItems={setItems}
-                    searchBy="36"
-                    textContent="36"
-                  />
-                  <SizeFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="size"
-                    setItems={setItems}
-                    searchBy="38"
-                    textContent="38"
-                  />
-                  <SizeFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="size"
-                    setItems={setItems}
-                    searchBy="40"
-                    textContent="40"
-                  />
-                </ul>
-              </Styled.SizeFilters>
-
-              <Styled.ColorFilters>
-                <p>Cor</p>
-                <ul>
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Vermelho"
-                    textContent="Vermelho"
-                  />
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Verde"
-                    textContent="Verde"
-                  />
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Chumbo"
-                    textContent="Chumbo"
-                  />
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Azul"
-                    textContent="Azul"
-                  />
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Marinho"
-                    textContent="Marinho"
-                  />
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Bege"
-                    textContent="Bege"
-                  />
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Marrom"
-                    textContent="Marrom"
-                  />
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Branco"
-                    textContent="Branco"
-                  />
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Preto"
-                    textContent="Preto"
-                  />
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Amarelo"
-                    textContent="Amarelo"
-                  />
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Pink"
-                    textContent="Pink"
-                  />
-                  <ColorFilter
-                    setTimeToDisplayItems={setTimeToDisplayItems}
-                    _timeToDisplayItems={timeToDisplayItems}
-                    filterOf="color"
-                    setItems={setItems}
-                    searchBy="Marinho"
-                    textContent="Marinho"
-                  />
-                </ul>
-              </Styled.ColorFilters>
-
-              <Styled.FormSearchByPrice>
-                <p>Preço</p>
-                <FormSearchByPrice />
-              </Styled.FormSearchByPrice>
-            </div>
-          </Styled.FilterSection>
+          <FiltersTemplate
+            setItems={setItems}
+            timeToDisplayItems={timeToDisplayItems}
+            setTimeToDisplayItems={setTimeToDisplayItems}
+          />
 
           {timeToDisplayItems ? (
             <ItemsExposition itemsList={items} />
---
+++ src/templates/filters/index.tsx
@@ -0,0 +1,285 @@
+import { Dispatch, SetStateAction } from 'react';
+
+import { ItemsList } from '@templates/itemsExposiotion';
+
+import { SizeFilter } from '@components/filters/size';
+import { BrandFilter } from '@components/filters/brand';
+import { ColorFilter } from '@components/filters/color';
+import { FormSearchByPrice } from '@components/searchByPrice';
+
+import * as Styled from './styled';
+
+export type FiltersTemplateProps = {
+  timeToDisplayItems: boolean;
+  setItems: Dispatch<SetStateAction<ItemsList>>;
+  setTimeToDisplayItems: Dispatch<SetStateAction<boolean>>;
+};
+
+export function FiltersTemplate({
+  setItems,
+  timeToDisplayItems,
+  setTimeToDisplayItems,
+}: FiltersTemplateProps): JSX.Element {
+  return (
+    <Styled.FilterSection>
+      <span>Filtros |</span>
+
+      <div>
+        <Styled.BrandFilters>
+          <p>Marca</p>
+          <ul>
+            <BrandFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              setItems={setItems}
+              filterOf="brand"
+              searchBy="Nike"
+              textContent="Nike"
+            />
+            <BrandFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              setItems={setItems}
+              filterOf="brand"
+              searchBy="Adidas"
+              textContent="Adidas"
+            />
+            <BrandFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              setItems={setItems}
+              filterOf="brand"
+              searchBy="Vr"
+              textContent="Vr"
+            />
+            <BrandFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              setItems={setItems}
+              filterOf="brand"
+              searchBy="Asics"
+              textContent="Asics"
+            />
+            <BrandFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              setItems={setItems}
+              filterOf="brand"
+              searchBy="Oakley"
+              textContent="Oakley"
+            />
+            <BrandFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              setItems={setItems}
+              filterOf="brand"
+              searchBy="Mizuno"
+              textContent="Mizuno"
+            />
+            <BrandFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              setItems={setItems}
+              filterOf="brand"
+              searchBy="Skechers"
+              textContent="Skechers"
+            />
+            <BrandFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              setItems={setItems}
+              filterOf="brand"
+              searchBy="Mizuno"
+              textContent="Mizuno"
+            />
+            <BrandFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              setItems={setItems}
+              filterOf="brand"
+              searchBy="Olympikus"
+              textContent="Olympikus"
+            />
+          </ul>
+        </Styled.BrandFilters>
+
+        <Styled.SizeFilters>
+          <p>Tamanho</p>
+          <ul>
+            <SizeFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="size"
+              setItems={setItems}
+              searchBy="26"
+              textContent="26"
+            />
+            <SizeFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="size"
+              setItems={setItems}
+              searchBy="28"
+              textContent="28"
+            />
+            <SizeFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="size"
+              setItems={setItems}
+              searchBy="30"
+              textContent="30"
+            />
+            <SizeFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="size"
+              setItems={setItems}
+              searchBy="32"
+              textContent="32"
+            />
+            <SizeFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="size"
+              setItems={setItems}
+              searchBy="34"
+              textContent="34"
+            />
+            <SizeFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="size"
+              setItems={setItems}
+              searchBy="36"
+              textContent="36"
+            />
+            <SizeFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="size"
+              setItems={setItems}
+              searchBy="38"
+              textContent="38"
+            />
+            <SizeFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="size"
+              setItems={setItems}
+              searchBy="40"
+              textContent="40"
+            />
+          </ul>
+        </Styled.SizeFilters>
+
+        <Styled.ColorFilters>
+          <p>Cor</p>
+          <ul>
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Vermelho"
+              textContent="Vermelho"
+            />
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Verde"
+              textContent="Verde"
+            />
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Chumbo"
+              textContent="Chumbo"
+            />
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Azul"
+              textContent="Azul"
+            />
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Marinho"
+              textContent="Marinho"
+            />
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Bege"
+              textContent="Bege"
+            />
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Marrom"
+              textContent="Marrom"
+            />
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Branco"
+              textContent="Branco"
+            />
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Preto"
+              textContent="Preto"
+            />
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Amarelo"
+              textContent="Amarelo"
+            />
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Pink"
+              textContent="Pink"
+            />
+            <ColorFilter
+              setTimeToDisplayItems={setTimeToDisplayItems}
+              _timeToDisplayItems={timeToDisplayItems}
+              filterOf="color"
+              setItems={setItems}
+              searchBy="Marinho"
+              textContent="Marinho"
+            />
+          </ul>
+        </Styled.ColorFilters>
+
+        <Styled.FormSearchByPrice>
+          <p>Preço</p>
+          <FormSearchByPrice />
+        </Styled.FormSearchByPrice>
+      </div>
+    </Styled.FilterSection>
+  );
+}
