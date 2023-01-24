2e76e6bd2b8d7bed288f1cdedf5ddcfa335e65fc
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-01-24 14:16:45 -0300

    feat: provendo novo state global para as paginas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
@@ -8,18 +9,27 @@ import { CartProvider } from '@contexts/shoppingCart/cart';
 import { ItemsBySearchProvider } from '@contexts/itemsBySearchForm';
 
 import { Header } from '@components/header';
+import { Footer } from '@components/footer';
 
 import { theme } from '@styles/theme';
 import { GlobalStyles } from '@styles/global-styles';
-import { Footer } from '@components/footer';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const [menuMobileVisible, setMenuMobileVisible] = useState(false);
+
   return (
     <ThemeProvider theme={theme}>
       <CartProvider>
         <ItemsBySearchProvider>
-          <Header />
-          <Component {...pageProps} />
+          <Header
+            menuMobileVisible={menuMobileVisible}
+            setMenuMobileVisible={setMenuMobileVisible}
+          />
+          <Component
+            {...pageProps}
+            menuMobileVisible={menuMobileVisible}
+            setMenuMobileVisible={setMenuMobileVisible}
+          />
           <Footer />
         </ItemsBySearchProvider>
       </CartProvider>
